943e522209e915c285e396e95fa42b468c760b69
Lasse Jöran Ermoneit <lasse.j.ermoneit@campus.tu-berlin.de>
2020-02-28 18:25:46 +0100

    test_adjoint working

--- test_adjoint.py
+++ test_adjoint.py
@@ -4,51 +4,97 @@ import matplotlib.pyplot as pl
 # dx/dt = -a * x + b * x(t-tau)
 
 class p:
-	a1 = 1.0
-	a2 = 1.0
-	b = 0.5
+	a = 0.0
+	K = 0.2
+	D = 0.0
+	J = 3.0
+	T = 1.0
 
-def derive(X, now, delay, p):
-	dx[0] = X[now][1]
-	dx[1] = -p.a1 * X[now][0]
+def derive(X, tp, p, t):
+	dx = np.zeros(3)
 	
-	# + p.b * X[delay]
+	dx[0] = X[tp[0]][0] * X[tp[0]][2]
+	dx[0]+= -p.a * X[tp[0]][1] * X[tp[0]][2]
+	dx[0]+= p.K * X[tp[1]][0]
+	dx[0]+= p.D
+	
+	dx[1] = X[tp[0]][1] * X[tp[0]][2]
+	dx[1]+=  p.a * X[tp[0]][0] * X[tp[0]][2]
+	dx[1]+= p.K * X[tp[1]][1]
+	dx[1]+= p.D
+	
+	dx[2] = (p.J - X[tp[0]][2] - (X[tp[0]][1] + 1.0) * (X[tp[0]][0]*X[tp[0]][0]+X[tp[0]][1]*X[tp[0]][1]))/p.T
+	
 	return dx
 
-ic_x = 1.0
+def derive_inearized(X, Y, tp, p, t):
+
+
+
+
+
+
+
+
+
+def hist_init(X, ic_X, opt):
+	if opt == "const":
+		for i in range(0, len(X)):
+			for j in range(0, len(X[0])):
+				X[i][j] = ic_X[j]
+
+ic_x0 = 1.0
+ic_x1 = 1.0
+ic_x2 = 1.0
+ic_X = [ic_x0, ic_x1, ic_x2]
+
 dt = 1e-3
 t_end = 400.0
+t_out = 400.0
+
 
 it = int(t_end/dt)
 
 tau = 1.0
 hist = int(tau/dt)
 
-now = 0
-delay = hist
+t = 0.0
+tp = [0, hist]
 
-X = np.full(hist+1, ic_x)
+X = np.zeros((hist+1, 3))
+hist_init(X, ic_X, "const")
 
-RES = []
-t = 0
+
+RES = np.zeros((int(t_out/dt), 4))
 
 for i in range(0, it):
 	
-	xnew = X[now] + dt * derive(X, now, delay, p)
+	xnew = X[tp[0]] + dt * derive(X, tp, p, t)
 	
-	RES.append([t, xnew])
+	X[tp[1]] = xnew
 	
-	
-	X[delay] = xnew
-	
-	
-	now = delay
-	delay = (delay + 1)%hist
-	
+	tp[0] = tp[1]
+	tp[1] = (tp[1] + 1)%hist
 	
 	t += dt
 
-RES = list(zip(*RES))
+	xnew = np.insert(xnew, 0, t)
+	
 
-pl.plot(RES[0], RES[1])
+	RES[i] = xnew
+
+
+
+
+RES = np.asarray(list(zip(*RES)))
+
+
+#pl.plot(RES[1], RES[2])
+
+I = RES[1]*RES[1]+RES[2]*RES[2]
+
+
+pl.plot(RES[0], I)
+pl.plot(RES[0], RES[3])
+
 pl.show()
