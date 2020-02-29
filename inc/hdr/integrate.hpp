8182fe6eaf0dec47bf0c12071fe9561649ca6e22
Lasse Jöran Ermoneit <lasse.j.ermoneit@campus.tu-berlin.de>
2020-02-29 18:32:22 +0100

    update

--- inc/hdr/integrate.hpp
+++ inc/hdr/integrate.hpp
@@ -4,6 +4,17 @@
 #include "../hdr/parameter.hpp"
 #include "../hdr/variable.hpp"
 
+class initial_condition
+{
+public:
+	std::vector<var> X;
+	double t;
+	
+	initial_condition();
+};
+
+//###########################################
+
 class timeseries
 {
 public:
@@ -21,6 +32,7 @@ class integrator
 public:
 	std::vector<varC> C;
 	std::vector<var> R;
+	allpar_set *AP;
 	long it;
 	long dim1;
 	long dim2;
@@ -30,6 +42,12 @@ public:
 	double t;
 	
 	integrator(allpar_set *AP);
+	
+	timeseries integrate(std::string opt);
+	void initialize(std::vector<varC> &C, std::string opt);
+	void initialize(std::vector<var> &R, std::string opt);
+	void initialize(std::vector<varC> &C, initial_condition ic);
+	void initialize(std::vector<var> &R, initial_condition ic);
 };
 
 //###########################################
--- inc/src/integrate.cpp
+++ inc/src/integrate.cpp
@@ -10,29 +10,102 @@
 
 using namespace std;
 
+
+initial_condition::initial_condition()
+{
+	
+}
+
+//###########################################
+
+timeseries::timeseries()
+{
+	
+}
+
+//###########################################
+
 integrator::integrator(allpar_set *AP)
 {
-	lpar_dbl_set lp(*AP);
-	fpar_dbl_set fp(*AP);
-	ipar_dbl_set ip(*AP);
+	this->AP = AP;
+	
+	lpar_dbl_set lp(*this->AP);
+	fpar_dbl_set fp(*this->AP);
+	ipar_dbl_set ip(*this->AP);
 
 	it = (long)(ip.int_time / ip.dt);
 
-	dim1 = (long)floor(lp.T / ip.dt );
-	dim2 = (long)floor(fp.tau / ip.dt + 1.0);
+	dim1 = (long)(lp.T / ip.dt  + 0.5);
+	dim2 = (long)(fp.tau / ip.dt + 0.5);
 
 	pos0 = dim1 - 1;
 	pos1 = pos0 - dim1;
 	pos2 = 0;
 	
 	t = 0.0;
-	
-	C.resize(dim2);
-	
-
-	
-	
-	
-	cout << "it" << '\t' << "dim1" << '\t' << "dim2" << endl;
-	cout << it << '\t' << dim1 << '\t' << dim2 << endl;
 }
+
+void integrator::initialize(vector<varC> &C, string opt)
+{
+	if(opt == "ones")
+	{
+		for(unsigned long i = 0; i < C.size(); i++)
+		{
+			C[i].E = 1.0 + 1.0i;
+			C[i].G = 1.0;
+			C[i].Q = 1.0;
+			C[i].J = 1.0;
+		}
+	}
+}
+
+
+timeseries integrator::integrate(string opt)
+{
+	timeseries TS;
+	
+	if(opt == "complex")
+	{
+		lpar_dbl_set lp(*this->AP);
+		fpar_dbl_set fp(*this->AP);
+		ipar_dbl_set ip(*this->AP);
+
+		this->C.resize(dim2);
+		
+		initialize(this->C, "ones");
+		
+		long res_size = long(ip.out_time / ip.dt + 0.5);
+		
+		TS.X.resize(res_size);
+		TS.t.resize(res_size);
+		TS.I.resize(res_size);
+		
+		for(long i = 0; i < (it - res_size); i++)
+		{
+			
+		}
+
+		for(long i = 0; i < res_size; i++)
+		{
+			
+		}
+		
+		
+	}
+	else if(opt == "linearized")
+	{
+		
+	}
+	else if(opt == "adjoint")
+	{
+		
+	}
+	else
+	{
+		cout << "err003" << endl;
+	}
+	return TS;
+}
+
+
+
