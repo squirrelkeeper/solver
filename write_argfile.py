d5e05c9f27a030a52db93cd80825c31752824616
squirrelkeeper <lasse.ermoneit@gmail.com>
2020-08-14 23:31:29 +0200

    writefile

--- write_argfile.py
+++ write_argfile.py
@@ -1,12 +1,6 @@
 import numpy as np
 
-
-
-
-
-other_pars = ""
-
-pts = 100
+pts = 101
 
 par1_name = "K"
 par1_start = 0.0
@@ -19,103 +13,66 @@ par2_name = "tau"
 par2_start = 0.0
 par2_stop = 5.0
 
-
-
 par2 = np.linspace(par2_start, par2_stop, pts)
 
 
 
+
 pxl_per_job = 10
-jobs_per_line = int(np.ceil(pts/pxl_per_job))
 jobs_per_arfile = 1000
 
-pxl = pts*pts
-jobs = int(np.ceil(pts/pxl_per_job)*pts)
 
-argfile_num = int(np.ceil(jobs/1000))
+base_str1 = "-m:lscan[" + par1_name + ","
+base_str2 = ","
+base_str3 = "," + str(pxl_per_job) + "," + par2_name + "] -" + par2_name + " "
+base_str4 = " -rea 100 -int_time 1500 -out_time 100"
 
 
 
+start_list = []
+stop_list = []
 
-args_list = []
+start = par1_start
+stop  = par1_start
 
+
+for i in range(pts):
+	stop += par1_incr
+	if i%pxl_per_job == pxl_per_job-1:
+		start_list.append(start)
+		stop_list.append(stop)
+		start = stop
+	elif i == pts -1:
+		start_list.append(start)
+		stop_list.append(par1_stop)
+
+
+
+str_list = []
 
 for j in range(pts):
-	for i in range(jobs_per_line):
-		this_start = pxl_per_job * i * par1_incr
-		this_stop = pxl_per_job * (i+1) * par1_incr
-		this_pts = pxl_per_job
-		if i == jobs_per_line-1:
-			this_start = pxl_per_job * i * par1_incr
-			this_stop = par1_stop
-			this_pts = pts%pxl_per_job
-		if this_pts != 0:
-			args = "-m:lscan["
-			args += par1_name
-			args += ","
-			args += str(this_start)
-			args += ","
-			args += str(this_stop)
-			args += ","
-			args += str(this_pts)
-			args += ","
-			args += par2_name
-			args += "] -"
-			args += par2_name
-			args += " "
-			args += str(j*(par2_stop-par2_start)/pts + par2_start)
-			args += " -rea "
-			args += str(100)
-			args += " -int_time "
-			args += str(1500)
-			args += " -out_time "
-			args += str(1000)
-			args += '\n'
-			args_list.append(args)
-
-
-args = ""
-
-
-
-
+	for i in range(len(start_list)):
+		s = base_str1 + str(start_list[i])
+		s+= base_str2 + str(stop_list[i])
+		s+= base_str3 + str(par2[j])
+		s+= base_str4
+		str_list.append(s)
 
 argfile_list = []
 
+arg_str = ""
 
-for j in range(argfile_num):
-	for i in range(jobs_per_arfile):
-		if i + jobs_per_arfile * j == len(args_list)-1:
-			break
-		args += args_list[i + jobs_per_arfile * j]
-	argfile_list.append(args)
-	args = ""
-
+for i in range(len(str_list)):
+	arg_str += str_list[i]
+	arg_str += '\n'
+	if i%jobs_per_arfile == jobs_per_arfile-1:
+		argfile_list.append(arg_str)
+		arg_str = ""
+	elif i == len(str_list) - 1:
+		argfile_list.append(arg_str)
 
 
 for i in range(len(argfile_list)):
 	f = open("argfile"+str(i), "w")
 	f.write(argfile_list[i])
 	f.close()
-
-
-
-'''
-for j in range(argfile_num):
-	args = ""
-	for i in range(jobs_per_arfile):
-		if i+j*jobs_per_arfile == len(args_list) - 1:
-			break
-		args += args_list[i+j*jobs_per_arfile]
-		if i == len(args_list)-1:
-			break
-	
-
-'''	
-
-
-
-	
-
-
-
